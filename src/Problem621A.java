1b169536de913a0f0465d43a3382048306aa7230
Mohammad Haider <haidmoham@gmail.com>
2016-07-20 18:58:19 -0400

    Adding solutions to 136A and 621A

---
+++ src/Problem621A.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Mohammad Haider (haidmoham) on 7/20/16.
+ */
+import java.util.*;
+
+public class Problem621A {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        long minOdd = Integer.MAX_VALUE;
+        long[] num = new long[n];
+        long sum = 0;
+        for (int i = 0; i < n; i++){
+            num[i] = s.nextLong();
+        }
+        for (int i = num.length-1; i >= 0; i--){
+            if (num[i]%2 == 1 && num[i] < minOdd)
+                minOdd = num[i];
+            sum += num[i];
+        }
+        if (sum%2 == 1)
+            sum -= minOdd;
+        System.out.println(sum);
+    }
+}
