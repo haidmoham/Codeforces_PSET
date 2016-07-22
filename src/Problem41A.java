038643df429e802b577d075a4dff104dcc579b44
Mohammad Haider <haidmoham@gmail.com>
2016-07-22 01:01:55 -0400

    Adding solution to Problem 41A

---
+++ src/Problem41A.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Mohammad Haider (haidmoham) on 7/22/16.
+ */
+import java.util.*;
+public class Problem41A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String t = sc.next();
+        String rs = new StringBuilder(s).reverse().toString();
+        if (rs.equals(t))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
