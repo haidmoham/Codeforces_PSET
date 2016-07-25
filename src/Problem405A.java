98dc13156ba9ddb651b882ebe83c25b0216f8482
haidmoham <haidmoham@gmail.com>
2016-07-25 14:01:54 -0400

    Adding solution to Problem 405A

---
+++ src/Problem405A.java
@@ -0,0 +1,18 @@
+/**
+ * Created by mohammad on 7/25/16.
+ */
+import java.util.*;
+
+public class Problem405A {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] in = new int[n];
+        for (int i = 0; i < n; i++){
+            in[i] = s.nextInt();
+        }
+        Arrays.sort(in);
+        for (int i = 0; i < n; i++)
+            System.out.print(in[i] + " ");
+    }
+}
