ffc2010920a142edae4c7b771b8fe580c2900144
haidmoham <haidmoham@gmail.com>
2016-07-26 15:42:53 -0400

    Adding solution to Problem230A

---
+++ src/Problem230A.java
@@ -0,0 +1,32 @@
+/**
+ * Created by mohammad on 7/25/16.
+ */
+import java.util.*;
+
+public class Problem230A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++){
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++){
+            if (s > arr[i][0]){
+                s += arr[i][1];
+                arr[i] = arr[n-1];
+                i = -1;
+                n--;
+            }
+        }
+        if (n == 0)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
