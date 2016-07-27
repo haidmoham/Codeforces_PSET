04741e73a36c474509c526d740d1cd89b0a35b1f
haidmoham <haidmoham@gmail.com>
2016-07-27 17:30:38 -0400

    Adding solution to Problem443A

---
+++ src/Problem443A.java
@@ -0,0 +1,20 @@
+/**
+ * Created by mohammad on 7/27/16.
+ */
+import java.util.*;
+public class Problem443A {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String in = s.nextLine();
+        in = in.replace('{',' ');
+        in = in.replace('}',' ');
+        in = in.replace(',',' ');
+        in = in.replaceAll("\\s+","");
+
+        Set resultSet = new HashSet();
+        for (int i = 0; i < in.length(); i++){
+            resultSet.add(new Character(in.charAt(i)));
+        }
+        System.out.println(resultSet.size());
+    }
+}
