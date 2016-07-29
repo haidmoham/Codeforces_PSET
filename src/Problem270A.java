87f494b33ef076bb82db2d50d2b6ddc289182578
haidmoham <haidmoham@gmail.com>
2016-07-29 01:19:27 -0400

    Adding solution to Problem270A

---
+++ src/Problem270A.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Problem270A {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for (int i = 0; i < n; i++) {
+            int th = s.nextInt();
+            if (360 % (180 - th) == 0) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
