02ee55f0631ce4f56ef315ea3472d56bbcf2e6c7
haidmoham <haidmoham@gmail.com>
2016-07-28 22:13:07 -0400

    Adding solution to Problem320A

---
+++ src/Problem320A.java
@@ -0,0 +1,12 @@
+/**
+ * Created by mohammad on 7/28/16.
+ */
+import java.util.*;
+
+public class Problem320A {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String in = s.next();
+        System.out.println(in.matches("(1|14|144)+") ? "YES" : "NO");
+    }
+}
