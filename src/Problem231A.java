af883921eec52871546023084ff5fad3390ef4fe
haidmoham <haidmoham@gmail.com>
2016-08-23 14:27:42 -0400

    Fix for 231A

--- src/Problem231A.java
+++ src/Problem231A.java
@@ -1,26 +1,20 @@
+/**
+ * Created by mohammad on 8/23/16.
+ */
 import java.util.*;
 
 public class Problem231A {
 	public static void main(String[] args){
-		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		n++;
-		String[] ar = new String[n];
- 		for (int i = 0; i < n; i++)
-		{
-			ar[i] = s.nextLine();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int count = 0;
+		for (int i = 0; i < n; i++){
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			if (a + b + c >= 2)
+				count++;
 		}
- 		s.close();
- 		int count = 0;
- 		String acc1 = "0 1 1";
- 		String acc2 = "1 0 1";
- 		String acc3 = "1 1 0";
- 		String acc4 = "1 1 1";
- 		for (int i = 0; i < n; i++)
- 		{
- 			if (ar[i].equals(acc1) || ar[i].equals(acc2) || ar[i].equals(acc3) || ar[i].equals(acc4))
- 				count++;
- 		}
- 		System.out.println(count);
+		System.out.println(count);
 	}
 }
