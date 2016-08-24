04356a1a4dc50ace896e283a55e6c23f125a97c0
haidmoham <haidmoham@gmail.com>
2016-08-23 21:55:41 -0400

    Fixing 455A

--- src/Problem455A.java
+++ src/Problem455A.java
@@ -7,13 +7,13 @@ public class Problem455A {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        long[] a = new long[10001];
+        long[] a = new long[100001];
         for (int i = 0; i < n; i++){
             int value = sc.nextInt();
             a[value] += value;
         }
         long f0 = 0, f1 = 0;
-        for (int i = 1; i < 10001; i++)
+        for (int i = 1; i < 100001; i++)
             f1 = Math.max(f0 + a[i], f0 = f1);
         System.out.println(f1);
     }
