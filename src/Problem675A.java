b672518b9825c81217c8b4c409cd882d74e45038
haidmoham <haidmoham@gmail.com>
2016-09-01 23:49:55 -0400

    675a

--- src/Problem675A.java
+++ src/Problem675A.java
@@ -7,17 +7,13 @@ public class Problem675A {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         long a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
-        if (b > 0) {
-            while (!(a >= b)) {
-                a += c;
-            }
+        if (c == 0) {
+            if (a == b) System.out.println("YES");
+            else System.out.println("NO");
         }
         else {
-            while (!(a <= b)) {
-                a += c;
-            }
+            if ((b - a) % c == 0 && (b - a)/ c >= 0) System.out.println("YES");
+            else System.out.println("NO");
         }
-        if (a == b) System.out.println("YES");
-        else System.out.println("NO");
     }
 }
