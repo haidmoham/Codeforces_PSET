096831ac549d8bb81621cede8488c23b0bd1f4c5
Eric Williamson <ericrw96@vt.edu>
2016-09-06 22:25:15 -0400

    Update Problem520B.java

--- src/Problem520B.java
+++ src/Problem520B.java
@@ -1,5 +1,6 @@
 /**
  * Created by mo on 9/6/16.
+ * Editing by Eric Williamson to try to generate nodes on the fly
  */
 import java.util.*;
 
@@ -7,48 +8,59 @@ public class Problem520B {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), m = sc.nextInt();
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        int iterate = 1000;
-        for (int i = 0; i < iterate; i++)
-            graph.add(new ArrayList<>());
-
-        for (int i = n; i < iterate; i++){
-            int root = n;
-
-            int u = n - 1;
-            int v = n * 2;
-            graph.get(root).add(u);
-            graph.get(u).add(root);
-
-            graph.get(root).add(v);
-            graph.get(v).add(root);
-        }
-        System.out.println(bfs(graph, n, m));
+        System.out.println(bfs(n, m));
     }
 
-    static int bfs(ArrayList<ArrayList<Integer>> graph, int start, int end){
-        int[] dist = new int[graph.size()];
+    static long bfs(int start, int end){
+        State startState = new State(0,start);
+        Queue<State> queue = new ArrayDeque<>();
+        Set<State> visited = new HashSet<>();
 
-        Queue<Integer> queue = new ArrayDeque<>();
-        Set<Integer> visited = new HashSet<>();
-
-        queue.offer(start);
-        visited.add(start);
-        dist[start] = 0;
+        queue.offer(startState);
+        visited.add(startState);
 
         while(!queue.isEmpty()){
-            int current = queue.poll();
-            int d = dist[current];
-            if(current == end) return d;
-
-            for (int adj : graph.get(current)){
+            State current = queue.poll();
+            if(current.number == end) return current.dist; 
+            for (State adj : current.adj(end)){
                 if (!visited.contains(adj)){
                     visited.add(adj);
                     queue.offer(adj);
-                    dist[adj] = d+1;
                 }
             }
         }
         return -1;
     }
+    
+    
+}
+class State{
+	long dist, number;
+	public State(long dist, long num){
+		this.dist = dist;
+		this.number = num;
+	}
+	
+	public ArrayList<State> adj(int end){
+	  ArrayList<State> s = new ArrayList<>(2);
+	  if (this.number < end){
+		  s.add(new State(dist + 1, number * 2));
+	  }
+	  if (this.number > 0){
+		  s.add(new State(dist + 1, number - 1));
+	  }
+	  return s;
+	}
+	@Override
+	public boolean equals(Object o){
+		if (o instanceof State){
+			State other = (State)o;
+			return other.dist == this.dist && other.number == this.number;
+		}
+		return false;
+	}
+	@Override
+	public int hashCode(){
+		return (int) (this.number + this.dist * 1000); 
+	}
 }
