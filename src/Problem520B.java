828f8d623a954673eefd608cdc02734e4e34771d
Mohammad Haider <haidmoham@gmail.com>
2016-09-07 13:54:23 -0400

    520B BFS (incomplete) and Greedy (Peter's, complete)

--- src/Problem520B.java
+++ src/Problem520B.java
@@ -7,48 +7,28 @@ public class Problem520B {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), m = sc.nextInt();
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        int iterate = 1000;
-        for (int i = 0; i < iterate; i++)
-            graph.add(new ArrayList<>());
+        ArrayDeque<Integer> bfs = new ArrayDeque<>();
+        bfs.add(n);
+        int[] best = new int[(int) 2e15];
+        best[n] = 0;
+        int steps = 0;
 
-        for (int i = n; i < iterate; i++){
-            int root = n;
+        while (bfs.size() > 0){
+            n = bfs.pop();
+            steps = best[n];
+            if (n == m){
+                System.out.println(steps);
+                break;
+            }
 
-            int u = n - 1;
-            int v = n * 2;
-            graph.get(root).add(u);
-            graph.get(u).add(root);
-
-            graph.get(root).add(v);
-            graph.get(v).add(root);
-        }
-        System.out.println(bfs(graph, n, m));
-    }
-
-    static int bfs(ArrayList<ArrayList<Integer>> graph, int start, int end){
-        int[] dist = new int[graph.size()];
-
-        Queue<Integer> queue = new ArrayDeque<>();
-        Set<Integer> visited = new HashSet<>();
-
-        queue.offer(start);
-        visited.add(start);
-        dist[start] = 0;
-
-        while(!queue.isEmpty()){
-            int current = queue.poll();
-            int d = dist[current];
-            if(current == end) return d;
-
-            for (int adj : graph.get(current)){
-                if (!visited.contains(adj)){
-                    visited.add(adj);
-                    queue.offer(adj);
-                    dist[adj] = d+1;
-                }
+            if (n < 10000 && best[2*n] > steps + 1){
+                best[2*n] = steps + 1;
+                bfs.add(2*n);
+            }
+            if (n > 0 && best[n-1] > steps + 1){
+                best[n-1] = steps + 1;
+                bfs.add(n-1);
             }
         }
-        return -1;
     }
 }
