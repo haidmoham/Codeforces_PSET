2b9aa05054aaf8a34b07cfdab16a0a6e6eb97fe0
Mohammad Haider <haidmoham@gmail.com>
2016-09-14 10:11:08 -0400

    Problem 714A/B, Greedy soln to 520B

---
+++ src/Problem520BGreedy.java
@@ -0,0 +1,18 @@
+/**
+ * Created by mo on 9/6/16.
+ */
+import java.util.*;
+
+public class Problem520BGreedy {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+
+        int count = 0;
+        while (m > n) {
+            m = m / (2 - m % 2) + (m % 2);
+            count++;
+        }
+        System.out.println(count + n - m);
+    }
+}
